71be14e8c6e98e2cd03eef094e9d8099a6b59807
Raymond Leon <raymondleon@raymondleon.ca>
2015-11-12 11:17:28 -0500

    Getting closer to being able to use sessions...

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ var http = require('http');
 var fs = require('fs');
 var express = require('express');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+
 var app = express();
 
+app.use(
+  session(
+    {
+      secret: 'test',
+      resave: 'false',
+      saveUninitialized: 'false',
+      store: new MongoStore(
+        {
+          url:"mongodb://localhost/test"
+        }
+      )
+    }
+  )
+);
+
+
 app.get('/', function(req,res){
-  res.cookie('state', 0);
+  console.log(req.session);
+  req.session.a = 108;
   res.send('Hello express');
 });
 
@@ -15,26 +36,7 @@ var server = app.listen(3000, function() {
 
   console.log('Example listening on http://%s:%s', host,port);
 });
-/*
-var port = 8009;
-http.createServer(function(req,res){
-  var testFileContents;
-  try{
-    fs.readFile('/home/person/Desktop/freelancing/chatroom/setusername.html', function(err,data){
-      try{
-        if (err) throw err;
 
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(data);
-      }
-      catch(err){
-        console.log('Could not open file test.html');
-      }
-    });
-  }
-  catch(err){
-    console.log('Error, could not open setusername.html');
-  }
 
-}).listen(port);
-*/
+
+
